faeaa213ffda7388f2a9de646e4ddb69e28e6d0d
Yicheng Luo <yicheng.luo16@imperial.ac.uk>
2019-07-18 15:21:09 +0100

    Add scheduler to training

--- train_pong.py
+++ train_pong.py
@@ -19,26 +19,34 @@ transform = transforms.ToTensor()
 dataset = Dataset('data', transform=transform)
 sampler = Sampler(dataset)
 loader = torch.utils.data.DataLoader(dataset,
-                                     batch_size=16,
-                                     sampler=sampler,
+                                     batch_size=batch_size,
+                                     sampler=sampler, pin_memory=True
 )
 
 model = transporter.Transporter(
     feature_encoder, pose_regressor, refine_net
 )
-# model.cuda()
+model.cuda()
 
-optimizer = torch.optim.Adam(model.parameters(), 5e-4)
+optimizer = torch.optim.Adam(model.parameters(), 1e-3)
+scheduler = torch.optim.lr_scheduler.StepLR(
+    optimizer,
+    int(1e5),
+    gamma=0.95, last_epoch=-1)
 
+model.train()
 for itr, (xt, xtp1) in enumerate(loader):
-    # xt = xt.cuda()
-    # xtp1 = xtp1.cuda()
+    xt = xt.cuda()
+    xtp1 = xtp1.cuda()
     optimizer.zero_grad()
     reconstruction = model(xt, xtp1)
     loss = torch.nn.functional.mse_loss(reconstruction, xtp1)
     loss.backward()
 
     optimizer.step()
+    scheduler.step()
     if itr % 100 == 0:
-        print("Loss ", loss)
+        print("Itr ", itr, "Loss ", loss)
         torch.save(model.state_dict(), "model.pth")
+    if itr > 1e6:
+        break
